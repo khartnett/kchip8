5f4b97f38743eeb8e13908fb1729c9158426418c
Kieran Hartnett <khartnet@gmail.com>
2020-08-11 14:18:37 -0500

    updating descriptions to opcodes to double quotes

--- chip8.py
+++ chip8.py
@@ -83,6 +83,7 @@ def loadGame(gameFile):
     f = open(gameFile + ".ch8","rb")
     gameBytes = list(f.read())
     #gameBytes = [0x60, 0x05, 0xF0, 0x29, 0x60, 0x00, 0x61, 0x00, 0xd0, 0x15, 0x12, 0x28]
+
     firstNib = False
     for i, gameByte in enumerate(gameBytes) :
         memory[i + 512] = gameByte
@@ -264,77 +265,78 @@ def getOpcodeDesc(opcode):
     # Execute Opcode
     if decoded == 0x0000:
         if NN == 0x00E0: # 0x00E0: Clears the screen
-            return '0x00E0 Clears the screen'
+            return "0x00E0 Clears the screen"
         elif NN == 0x00EE: # 0x00EE: Returns from subroutine
-            return '0x00EE Returns from subroutine'
+            return "0x00EE Returns from subroutine"
     elif decoded == 0x1000: # 0x1NNN:	Jumps to address NNN.
-        return '0x1NNN Jumps to address NNN'
+        return "0x1NNN Jumps to address NNN"
     elif decoded == 0x2000: # 0x2NNN:	Calls subroutine at NNN.
-        return '0x2NNN Calls subroutine at NNN'
+        return "0x2NNN Calls subroutine at NNN"
     elif decoded == 0x3000: # 0x3XNN:	Skips the next instruction if VX equals NN.
-        return '0x3XNN Skips the next instruction if VX equals NN'
+        return "0x3XNN Skips the next instruction if VX equals NN"
     elif decoded == 0x4000: # 0x4XNN:	Skips the next instruction if VX doesn't equal NN.
         return "0x4XNN Skips the next instruction if VX doesn't equal NN"
     elif decoded == 0x5000: # 0x5XY0:	Skips the next instruction if VX equals VY.
-        return '0x5XY0 Skips the next instruction if VX equals VY'
+        return "0x5XY0 Skips the next instruction if VX equals VY"
     elif decoded == 0x6000: # 0x6XNN:	Sets VX to NN.
-        return '0x6XNN Sets VX to NN'
+        return "0x6XNN Sets VX to NN"
     elif decoded == 0x7000: # 0x7XNN:	Adds NN to VX. (Carry flag is not changed).
-        return '0x7XNN Adds NN to VX. (Carry flag is not changed)'
+        return "0x7XNN Adds NN to VX. (Carry flag is not changed)"
     elif decoded == 0x8000:
         if N == 0x0000: # 0x8XY0 Sets VX to the value of VY.
-            return '0x8XY0 Sets VX to the value of VY'
+            return "0x8XY0 Sets VX to the value of VY"
         elif N == 0x0001: # 0x8XY1 Sets VX to VX or VY. (Bitwise OR operation)
-            return '0x8XY1 Sets VX to VX or VY. (Bitwise OR operation)'
+            return "0x8XY1 Sets VX to VX or VY. (Bitwise OR operation)"
         elif N == 0x0002: # 0x8XY2 Sets VX to VX and VY. (Bitwise AND operation)
-            return '0x8XY2 Sets VX to VX and VY. (Bitwise AND operation)'
+            return "0x8XY2 Sets VX to VX and VY. (Bitwise AND operation)"
         elif N == 0x0003: # 0x8XY3 Sets VX to VX xor VY.
-            return '0x8XY3 Sets VX to VX xor VY'
+            return "0x8XY3 Sets VX to VX xor VY"
         elif N == 0x0004: # 0x8XY4 Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
             return "0x8XY4 Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't"
         elif N == 0x0005: # 0x8XY5 VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
             return "0x8XY5 VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't"
         elif N == 0x0006: # 0x8XY6 Stores the least significant bit of VX in VF and then shifts VX to the right by 1.
-            return '0x8XY6 Stores the least significant bit of VX in VF and then shifts VX to the right by 1'
+            return "0x8XY6 Stores the least significant bit of VX in VF and then shifts VX to the right by 1"
         elif N == 0x0007: # 0x8XY7 Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
             return "0x8XY7 Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't"
         elif N == 0x000E: # 0x8XYE Stores the most significant bit of VX in VF and then shifts VX to the left by 1.
-            return '0x8XYE Stores the most significant bit of VX in VF and then shifts VX to the left by 1'
+            return "0x8XYE Stores the most significant bit of VX in VF and then shifts VX to the left by 1"
     elif decoded == 0x9000: # 0x9XY0:	Skips the next instruction if VX doesn't equal VY.
         return "0x9XY0 Skips the next instruction if VX doesn't equal VY"
     elif decoded == 0xA000: # 0xANNN: Sets I to the address NNN
-        return '0xANNN Sets I to the address NNN'
+        return "0xANNN Sets I to the address NNN"
+        test
     elif decoded == 0xB000: # 0xBNNN: Jumps to the address NNN plus V0
-        return '0xBNNN Jumps to the address NNN plus V0'
+        return "0xBNNN Jumps to the address NNN plus V0"
     elif decoded == 0xC000: # 0xCXNN: Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN
-        return '0xCXNN Sets VX to the result of a bitwise and operation on a random number'
+        return "0xCXNN Sets VX to the result of a bitwise and operation on a random number and NN"
     elif decoded == 0xD000: # 0xDXYN Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels
-        return '0xDXYN Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels'
+        return "0xDXYN Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels"
     elif decoded == 0xE000:
         if NN == 0x009E: # 0xEX9E Skips the next instruction if the key stored in VX is pressed.
-            return '0xEX9E Skips the next instruction if the key stored in VX is pressed'
+            return "0xEX9E Skips the next instruction if the key stored in VX is pressed"
         elif NN == 0x00A1: # 0xEXA1 Skips the next instruction if the key stored in VX isn't pressed.
             return "0xEXA1 Skips the next instruction if the key stored in VX isn't pressed"
     elif decoded == 0xF000:
         if NN == 0x0007: # 0xFX07 Sets VX to the value of the delay timer.
-            return '0xFX07 Sets VX to the value of the delay timer'
+            return "0xFX07 Sets VX to the value of the delay timer"
         elif NN == 0x000A: # 0xFX0A A key press is awaited, and then stored in VX. (Blocking)
-            return '0xFX0A A key press is awaited, and then stored in VX'
+            return "0xFX0A A key press is awaited, and then stored in VX"
         elif NN == 0x0015: # 0xFX15 Sets the delay timer to VX.
-            return '0xFX15 Sets the delay timer to VX'
+            return "0xFX15 Sets the delay timer to VX"
         elif NN == 0x0018: # 0xFX18 Sets the sound timer to VX.
-            return '0xFX18 Sets the sound timer to VX'
+            return "0xFX18 Sets the sound timer to VX"
         elif NN == 0x001E: # 0xFX1E Adds VX to I. VF is not affected.
-            return '0xFX1E Adds VX to I. VF is not affected'
+            return "0xFX1E Adds VX to I. VF is not affected"
         elif NN == 0x0029: # 0xFX29 Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font.
-            return '0xFX29 Sets I to the location of the sprite for the character in VX. Characters 0-F'
+            return "0xFX29 Sets I to the location of the sprite for the character in VX. Characters 0-F"
         elif NN == 0x0033: # 0xFX33 Stores the Binary-coded decimal representation of VX at the addresses I, I plus 1, and I plus 2
-            return '0xFX33 Stores the Binary-coded decimal representation of VX at the addresses I, I plus 1, and I plus 2'
+            return "0xFX33 Stores the Binary-coded decimal representation of VX at the addresses I, I plus 1, and I plus 2"
         elif NN == 0x0055: # 0xFX55 Stores V0 to VX (including VX) in memory starting at address I
-            return '0xFX55 Stores V0 to VX (including VX) in memory starting at address I'
+            return "0xFX55 Stores V0 to VX (including VX) in memory starting at address I"
         elif NN == 0x0065: # 0xFX65 Fills V0 to VX (including VX) with values from memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodifie
-            return '0xFX65 Fills V0 to VX (including VX) with values from memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified'
-    return ('Unknown opcode: ' + hex(opcode))
+            return "0xFX65 Fills V0 to VX (including VX) with values from memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified"
+    return ("Unknown opcode: " + hex(opcode))
 
 
 def drawGraphics():
@@ -405,7 +407,7 @@ loadGame(gameFileName) #si') #'pong') ll kt
 print(" Keys: 'p'=play/pause '['=step 'o'=print opcode 'i'=print info 'm'=print memory 't'=terminate")
 #printIndexList(memory)
 #exit()
-printRunningOps = False
+printRunningOps = True
 while 1:
     if (runCycle or stepCycle) :
         emulateCycle()
